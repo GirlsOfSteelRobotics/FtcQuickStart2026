492ed1f40fb3b4e6da5d813eb950652afd7ff00d
Sarah Withee <2601974+geekygirlsarah@users.noreply.github.com>
2026-09-20 14:20:15 -0400

    Move Constants out of `pedro` directory and add some comment placeholders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,11 +1,18 @@
-package org.firstinspires.ftc.teamcode.pedro;
+package org.firstinspires.ftc.teamcode;
 
 import com.pedropathing.follower.Follower;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
+
+    // Insert drivetrain config here
+
+    // Insert localization config here
+
+    // Insert Foresignt config here
+
+    // Insert Follower create() here
     public static Follower create(HardwareMap h) {
-        // return new Follower(Drivetrain, Localizer, Foresight);
         return null;
     }
 }
