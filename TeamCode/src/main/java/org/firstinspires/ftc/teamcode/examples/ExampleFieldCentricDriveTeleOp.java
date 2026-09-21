67545d56af505e36b22c9f6cb067ff0cf7b061d4
Sarah Withee <2601974+geekygirlsarah@users.noreply.github.com>
2026-09-21 12:56:15 -0400

    Add example TeleOp modes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/ExampleFieldCentricDriveTeleOp.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.examples;
+
+import com.pedropathing.drivetrain.DrivePowers;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.ManualDrive;
+import com.pedropathing.math.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.Constants;
+
+public class ExampleFieldCentricDriveTeleOp extends OpMode {
+    private Follower follower;
+
+    @Override
+    public void init() {
+        follower = Constants.create(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        DrivePowers powers = ManualDrive.fieldCentric(
+                -gamepad1.left_stick_y,
+                gamepad1.left_stick_x,
+                gamepad1.right_stick_x,
+                follower.pose().heading()
+        );
+
+        follower.manual(powers);
+        follower.update();
+
+        telemetry.addData("X", follower.pose().x());
+        telemetry.addData("Y", follower.pose().y());
+        telemetry.addData("Heading", Math.toDegrees(follower.pose().heading()));
+        telemetry.addData("Follower Mode", follower.mode());
+        telemetry.update();
+    }
+
+}
